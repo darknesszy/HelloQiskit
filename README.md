f3d9494ff291740eabaf9dcd4ff792f4d77ee8ef
Michael <michaelziyumay@hotmail.com>
2020-05-29 17:06:21 +0800

    Added account and login token.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Overview
 
+## Environment Variables
+`IBMQ_TOKEN` = The token provided in IBM Quantum Experience's account page.
+
 # Libraries
 
 # Useful Resources
--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-import qiskit
+import os
+from qiskit import IBMQ, __version__
+from dotenv import load_dotenv
 
-print('hello qiskit')
+# Load environment variables.
+load_dotenv()
+
+# Login to IBM Quantum.
+if IBMQ.stored_account().get('token') == None:
+    IBMQ.save_account(os.environ['IBMQ_TOKEN'])
+    print('You are now logged in...')
+
+print(__version__)
